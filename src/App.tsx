e24ccb276d445f05faaf04b088417d52ef29f8eb
nikhilpanchabhai5 <nikhilpanchabhai@yahoo.com>
2026-06-19 18:09:42 +0530

    Initial React + Vite + TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#0a0f1e",
+        color: "white",
+        fontSize: "2rem",
+      }}
+    >
+      🚀 Niktevia is under construction...
+    </div>
+  );
+}
+
+export default App;
